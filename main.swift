a284ed8889d3041f9de680576b4556fe271f0a29
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-06-20 11:04:18 +0800

    1加到100
    首先安装好xcode7.31版本，打开Xcode，创建一个项目，打开OS X－Application-Command line
    TOOL，定义一个变量i给它赋值为1，再定义一个变量a赋值为0，用while循环进行1到100的相加，while后边是循环条件i<=100，括号
    里是循环语句，先把i＋a赋值给a，i自加。最后输出。

---
+++ main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  1add100
+//
+//  Created by 李晓晋 on 16/6/20.
+//  Copyright © 2016年 李晓晋. All rights reserved.
+//
+
+import Foundation
+var i=1
+var a=0
+while i<=100
+{
+    a=a+i
+    i++
+}
+print(a)
+
+
+
